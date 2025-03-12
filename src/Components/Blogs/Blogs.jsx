6a752cf25b8920bc73eaefe112df043cb2882a9b
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-03-12 14:22:43 +0600

    blogs component ready

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import whait from '../../assets/whait profile.jpg'
 
 const Blogs = () => {
     return (
         <div className='rounded w-1/4 bg-base-300'>
-            <h2>Blogs</h2>
+            <div className='flex justify-center items-center p-5 '>
+                <img className='w-32 h-32 rounded-full' src={whait} alt="" />
+                
+
+            </div>
         </div>
     );
 };
