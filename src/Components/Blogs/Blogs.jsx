8a38b53bfa2b923abc4f0847011c740ab668cea3
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-03-12 14:45:55 +0600

    blogs project 80% complite

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import whait from '../../assets/whait profile.jpg'
 
 const Blogs = () => {
+    
     return (
         <div className='rounded w-1/4 bg-base-300'>
             <div className=' p-5 '>
@@ -11,6 +12,11 @@ const Blogs = () => {
                 <div className='flex justify-center pt-2'>
                     <button className='border rounded-3xl py-1 px-3 font-semibold'>Follow me</button>
                 </div>
+            </div>
+            <div>
+                {
+
+                }
             </div>
         </div>
     );
